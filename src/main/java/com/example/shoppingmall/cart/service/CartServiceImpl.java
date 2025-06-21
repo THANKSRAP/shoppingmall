bb6ae45f3de71ed3f8c1e25ed5af24b949a578da
hyunji.na <rhcnwkd1279@gmail.com>
2025-06-22 01:25:59 +0900

    장바구니 기능 구현

---
+++ src/main/java/com/example/shoppingmall/cart/service/CartServiceImpl.java
@@ -0,0 +1,53 @@
+package com.example.shoppingmall.cart.service;
+
+import com.example.shoppingmall.cart.dao.CartDao;
+import com.example.shoppingmall.cart.domain.CartDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CartServiceImpl implements CartService {
+    @Autowired
+    private CartDao cartdao;
+
+    @Override
+    public List<CartDto> getCartByUserId(int userId) {
+        try {
+            System.out.println("== Fighting  ==");
+            return cartdao.selectCartByUserId(userId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void updateQuantity(int cartId, int quantity) {
+        try {
+            cartdao.updateQuantity(cartId, quantity);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteByCartIds(List<Integer> cartItemIds) {
+        try {
+            for (int id : cartItemIds) {
+                cartdao.deleteByCartId(id);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteAllByUserId(int userId) {
+        try {
+            cartdao.deleteAllByUserId(userId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
